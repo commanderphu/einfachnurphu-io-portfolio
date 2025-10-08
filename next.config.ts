3d81d118d625e6fc0952bdc087e114c9e54156ca
Phu <joshua@phuonline.de>
2025-10-08 12:22:40 +0200

    cfix: final cloudflare deploy config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import { withContentlayer } from "next-contentlayer"
-import { runtime } from "./app/api/og/route"
 
 const nextConfig = {
   eslint: {
-    // 🚀 verhindert, dass ESLint den Build auf CF blockiert
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
   reactStrictMode: true,
-  experimental:{runtime:"edge"}
+  // ❌ Kein globales runtime-Feld mehr
 }
 
 export default withContentlayer(nextConfig)
