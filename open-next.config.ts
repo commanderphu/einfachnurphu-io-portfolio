9aec27bc75ad456f0fc3d581ee9f930552567e38
Phu <joshua@phuonline.de>
2025-11-07 14:23:14 +0100

    fix: add complete middleware config to OpenNext

--- open-next.config.ts
+++ open-next.config.ts
@@ -9,4 +9,15 @@ export default {
       queue: "dummy",
     },
   },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 }
